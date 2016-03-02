af4f1a8009ee9cd13b6f6dbd3cc99495a4ea1816
Denis Defreyne <denis.defreyne@stoneship.org>
2016-03-02 22:35:40 +0100

    Crystallize

---
+++ othello.cr
@@ -0,0 +1,309 @@
+class Grid
+  def initialize(grid = nil)
+    @grid = grid || {
+      [3, 3] => :black,
+      [4, 4] => :black,
+      [4, 3] => :white,
+      [3, 4] => :white,
+    }
+  end
+
+  def [](x : Int32, y : Int32)
+    raise ArgumentError.new("x must be 0..7") unless (0..7).includes?(x)
+    raise ArgumentError.new("y must be 0..7") unless (0..7).includes?(y)
+
+    @grid[[x, y]]?
+  end
+
+  def [](coords : Array)
+    if coords.size != 2
+      raise ArgumentError.new("expected #coords to be called with two args")
+    else
+      self[coords[0], coords[1]]
+    end
+  end
+
+  def filled?
+    (0..7).to_a.zip((0..7).to_a) do |x, y|
+      return false if self[x, y].nil?
+    end
+    true
+  end
+
+  def count(color)
+    @grid.values.count { |v| v == color }
+  end
+
+  def valid_move?(move)
+    if self[[move.x, move.y]]
+      false
+    else
+      cast_rays(move.x, move.y).any? { |ray| valid_ray?(ray, move.color) }
+    end
+  end
+
+  def cast_rays(x, y)
+    diffs =
+      [
+        [1, 1],
+        [1, 0],
+        [1, -1],
+        [0, -1],
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, 1],
+      ]
+
+    diffs
+      .map { |ray| cast_ray(x, y, ray[0], ray[1]) }
+  end
+
+  def cast_ray(x, y, fx, fy)
+    bounds = (0..7)
+
+    dxs = (1..7).map { |i| i * fx }
+    dys = (1..7).map { |i| i * fy }
+
+    dxs.zip(dys)
+      .map { |pair| [x+pair[0], y+pair[1]] }
+      .select { |pair| bounds.includes?(pair[0]) && bounds.includes?(pair[1]) }
+  end
+
+  def valid_ray?(ray, color)
+    return false if ray.size < 2
+
+    chunks = [
+      [ray[0]],
+    ]
+
+    ray[1..-1].each do |pair|
+      if self[chunks.last.last] != self[pair]
+        chunks << [pair]
+      else
+        chunks.last << pair
+      end
+    end
+
+    chunks.size >= 2 && self[chunks[0][0]] == invert_color(color) && self[chunks[1][0]] == color
+  end
+
+  def invert_color(color)
+    case color
+    when :black
+      :white
+    when :white
+      :black
+    else
+      raise ArgumentError.new("invalid color: #{color}")
+    end
+  end
+
+  def valid_moves(color)
+    moves =
+      (0..7).flat_map do |x|
+        (0..7).map do |y|
+          Move.new(x, y, color)
+        end
+      end
+
+    moves.select { |m| valid_move?(m) }
+  end
+
+  def apply_move(move)
+    new_grid = @grid.merge({ [move.x, move.y] => move.color })
+
+    valid_rays =
+      cast_rays(move.x, move.y).select{ |ray| valid_ray?(ray, move.color) }
+    valid_rays.each do |ray|
+      ray.each do |pair|
+        break if @grid[pair] == move.color
+        new_grid[pair] = move.color
+      end
+    end
+
+    self.class.new(new_grid)
+  end
+
+  def to_s(io)
+    io << "   A  B  C  D  E  F  G  H"
+    io << "\n"
+    (0..7).each do |y|
+      io << "12345678"[y]
+      io << ' '
+      io << "\e[42m"
+      (0..7).each do |x|
+        io << ' '
+        io <<
+          case self[x, y]
+          when :black
+            '⚫'
+          when :white
+            '⚪'
+          else
+            ' '
+          end
+        io << ' '
+      end
+      io << "\e[0m\n"
+    end
+    io << "\n"
+  end
+end
+
+struct Move
+  getter :x
+  getter :y
+  getter :color
+
+  def initialize(x, y, color)
+    @x = x
+    @y = y
+    @color = color
+  end
+
+  def inspect
+    "Move(#{x}, #{y}, #{color})"
+  end
+
+  def to_s(io)
+    io << ('A'..'H').to_a[x] + ('0'..'7').to_a[y]
+  end
+end
+
+class Player
+  getter :color
+
+  def initialize(color)
+    @color = color
+  end
+
+  def next_move(grid)
+    raise "Not implemented"
+  end
+end
+
+class HumanPlayer < Player
+  def next_move(grid)
+    loop do
+      print "#{@color}’s move? (e.g. A3) "
+      raw_line = gets
+      if raw_line
+        line = raw_line.strip
+        if line !~ /\A(\w)(\d)\z/
+          puts "Invalid input: needs to be in the format “A3”."
+        elsif !('A'..'H').includes?($1.upcase) || !(1..8).includes?($2.to_i)
+          puts "Invalid input: needs to be in the format “A3”."
+        else
+          move = Move.new($1.upcase.ord - 'A'.ord, $2.to_i - 1, @color)
+
+          if grid.valid_move?(move)
+            break move
+          else
+            puts "Invalid move!"
+          end
+        end
+      else
+        puts "Invalid input."
+      end
+    end
+  end
+end
+
+class RandomPlayer < Player
+  def next_move(grid)
+    grid.valid_moves(color).sample
+  end
+end
+
+class AIPlayer < Player
+  def next_move(grid)
+    players = [
+      RandomPlayer.new(:white),
+      RandomPlayer.new(:black),
+    ]
+
+    grid.valid_moves(color).max_by do |move|
+      game = Game.new(SilentUI.new, grid.apply_move(move), players)
+      num_wins = (0...5).count { game.play == color }
+      num_wins
+    end
+  end
+end
+
+class UI
+  def before_move(player, grid)
+    raise "Not implemented"
+  end
+
+  def after_move(player, grid)
+    raise "Not implemented"
+  end
+
+  def announce_winner(color)
+    raise "Not implemented"
+  end
+end
+
+class HumanUI < UI
+  def before_move(player, grid)
+    puts grid
+  end
+
+  def after_move(player, grid)
+    sleep 0.1
+    puts
+  end
+
+  def announce_winner(color)
+    puts "#{color} wins!"
+  end
+end
+
+class SilentUI < UI
+  def before_move(player, grid)
+  end
+
+  def after_move(player, grid)
+  end
+
+  def announce_winner(color)
+  end
+end
+
+class Game
+  def initialize(ui : UI, grid = Grid.new, players = nil)
+    @ui = ui
+    @grid = grid
+
+    @players = players || [
+      RandomPlayer.new(:white),
+      AIPlayer.new(:black),
+    ]
+  end
+
+  def play
+    grid = @grid
+    loop do
+      @players.each do |player|
+        @ui.before_move(player, grid)
+
+        if grid.filled?
+          winner = grid.count(:white) > grid.count(:black) ? :white : :black
+          @ui.announce_winner(winner)
+          return winner
+        elsif grid.valid_moves(player.color).empty?
+          # FIXME: bad win condition
+          winner = grid.invert_color(player.color)
+          @ui.announce_winner(winner)
+          return winner
+        end
+
+        grid = grid.apply_move(player.next_move(grid))
+        @ui.after_move(player, grid)
+      end
+    end
+  end
+end
+
+p Game.new(HumanUI.new).play
